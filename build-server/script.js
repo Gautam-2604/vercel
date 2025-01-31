021899e104b4d7e368981714d322ae9bb89ebc95
Gautam-2604 <gaurisariag@gmail.com>
2025-01-31 18:32:56 +0530

    some changes

--- build-server/script.js
+++ build-server/script.js
@@ -1,61 +1,89 @@
-const {exec} = require('child_process')
+const { exec } = require('child_process')
 const path = require('path')
-const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
+const fs = require('fs')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 const mime = require('mime-types')
+const { Kafka } = require('kafkajs')
 
-const s3Client = new S3Client ({
-    region:'eu-north-1',
-    credentials:{
-        accessKeyId:'AKIA4HWJUDI6MAO5S4HB',
-        secretAccessKey:'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'
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
     }
 })
 
 const PROJECT_ID = process.env.PROJECT_ID
+const DEPLOYEMENT_ID = process.env.DEPLOYEMENT_ID
 
-const fs = require('fs')
+const kafka = new Kafka({
+    clientId: `docker-build-server-${DEPLOYEMENT_ID}`,
+    brokers: [''],
+    ssl: {
+        ca: [fs.readFileSync(path.join(__dirname, 'kafka.pem'), 'utf-8')]
+    },
+    sasl: {
+        username: '',
+        password: '',
+        mechanism: 'plain'
+    }
 
-async function init(){
-    console.log('Executing the build function');
+})
 
+const producer = kafka.producer()
+
+async function publishLog(log) {
+    await producer.send({ topic: `container-logs`, messages: [{ key: 'log', value: JSON.stringify({ PROJECT_ID, DEPLOYEMENT_ID, log }) }] })
+}
+
+async function init() {
+
+    await producer.connect()
+
+    console.log('Executing script.js')
+    await publishLog('Build Started...')
     const outDirPath = path.join(__dirname, 'output')
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
-    p.stdout.on('data', function(data){
-        console.log(data.toString());
+    p.stdout.on('data', function (data) {
+        console.log(data.toString())
+        publishLog(data.toString())
     })
 
-    p.stdout.on('error', function(data){
-        console.log(data.toString());
+    p.stdout.on('error', async function (data) {
+        console.log('Error', data.toString())
+        await publishLog(`error: ${data.toString()}`)
     })
 
-    p.on('close', async function(){
-        console.log("Build complete");
+    p.on('close', async function () {
+        console.log('Build Complete')
+        await publishLog(`Build Complete`)
         const distFolderPath = path.join(__dirname, 'output', 'dist')
-        const distFolderContents = fs.readdirSync(distFolderPath, {recursive: true})
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
 
-        for( const file of distFolderContents){
+        await publishLog(`Starting to upload`)
+        for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file)
+            if (fs.lstatSync(filePath).isDirectory()) continue;
 
-            if(fs.lstatSync(filePath).isDirectory())
-            continue;
-
-            console.log('uploading', filePath);
+            console.log('uploading', filePath)
+            await publishLog(`uploading ${file}`)
 
             const command = new PutObjectCommand({
-                Bucket:'vercel-ggaurisaria',
-                Key:`__outputs/${PROJECT_ID}/${file}`,
+                Bucket: 'vercel-clone-outputs-1',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
                 Body: fs.createReadStream(filePath),
                 ContentType: mime.lookup(filePath)
             })
 
             await s3Client.send(command)
-            console.log('uploaded', filePath);
-            
+            publishLog(`uploaded ${file}`)
+            console.log('uploaded', filePath)
         }
-
-        console.log('Done....');
+        await publishLog(`Done`)
+        console.log('Done...')
+        process.exit(0)
     })
 }
 
